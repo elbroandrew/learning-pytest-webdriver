1d02384c0fa8e456cb1eab1ebed6514d3a8a9c5d
Andrew <elbroandrew@gmail.com>
2021-12-08 18:25:09 +1000

    fix: change driver path

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,29 +6,27 @@ from selenium.webdriver.chrome.options import Options as chrome_options
 @pytest.fixture
 def get_chrome_options():
     options = chrome_options()
-    options.add_argument('chrome')  # Use headless if you don't need a browser UI.
+    options.add_argument('chrome')  # Use headless if you do not need a browser UI
     options.add_argument('--start-maximized')
-    options.add_argument('--window-size=800,600')
+    options.add_argument('--window-size=1650,900')
     return options
 
 
 @pytest.fixture
-def get_webdriver():
-    options = get_chrome_options()
+def get_webdriver(get_chrome_options):
+    options = get_chrome_options
     driver = webdriver.Chrome(
-        executable_path='C:\Users\Andrew\PycharmProjects\pytest-venv\pytest_youtube\chromedriver',
-        options=options
-    )
+        executable_path='chromedriver/chromedriver.exe',
+        options=options)
     return driver
 
-@pytest.fixture
+
+@pytest.fixture(scope='function')
 def setup(request, get_webdriver):
-    driver = get_webdriver()
-    url = 'https://www.macys.com'
+    driver = get_webdriver
+    url = 'https://www.macys.com/'
     if request.cls is not None:
         request.cls.driver = driver
     driver.get(url)
     yield driver
     driver.quit()
-
-
