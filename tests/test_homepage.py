766d3bc697d17c3e530b233cbe8e8ffe167934b9
Andrew <elbroandrew@gmail.com>
2021-12-09 18:26:25 +1000

    feat: add homepage class

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,8 +1,19 @@
 import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+
 
 
 @pytest.mark.usefixtures('setup')
 class TestHomepage:
 
     def test_homepage(self):
-        pass
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(2)
+        element1 = driver.find_element(By.CSS_SELECTOR, '#id123')
+
+        wait = WebDriverWait(driver, 15, 0.3)
+        element = wait.until(ec.visibility_of_element_located((By.CSS_SELECTOR, '#id123')))
+
