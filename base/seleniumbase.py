3bc453a50768431b7e7a4d09669c9b07c9859c4f
Andrew <elbroandrew@gmail.com>
2021-12-09 18:42:59 +1000

    feat: add selenium base class methods

--- base/seleniumbase.py
+++ base/seleniumbase.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.remote.webelement import WebElement
 
 
 class SeleniumBase:
@@ -8,7 +9,7 @@ class SeleniumBase:
         self.driver = driver
         self.wait = WebDriverWait(driver, 15, 0.3)
 
-    def get_selenium_by(self, find_by: str):
+    def get_selenium_by(self, find_by: str) -> dict:
         find_by = find_by.lower()
         locating = {'css': By.CSS_SELECTOR,
                     'xpath': By.XPATH,
@@ -22,4 +23,26 @@ class SeleniumBase:
 
         return locating[find_by]
 
-    print(get_selenium_by('css'))
+    def is_visible(self, find_by: str, locator: str, locator_name: str = None) -> WebElement:
+        return self.wait.until(ec.visibility_of_element_located((self.get_selenium_by(find_by), locator)), locator_name)
+
+    def is_present(self, find_by: str, locator: str, locator_name: str = None) -> WebElement:
+        return self.wait.until(ec.presence_of_element_located((self.get_selenium_by(find_by), locator)), locator_name)
+
+    def is_not_present(self, find_by: str, locator: str, locator_name: str = None) -> WebElement:
+        return self.wait.until(
+            ec.invisibility_of_element_located((self.get_selenium_by(find_by), locator)),
+            locator_name
+        )
+
+    def are_visible(self, find_by: str, locator: str, locator_name: str = None) -> WebElement:
+        return self.wait.until(
+            ec.visibility_of_all_elements_located((self.get_selenium_by(find_by), locator)),
+            locator_name
+        )
+
+    def are_present(self, find_by: str, locator: str, locator_name: str = None) -> WebElement:
+        return self.wait.until(
+            ec.presence_of_all_elements_located((self.get_selenium_by(find_by), locator)),
+            locator_name
+        )
