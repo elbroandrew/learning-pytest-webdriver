55b90405c28f94fb321787b7b64c131b07266413
Andrew <elbroandrew@gmail.com>
2021-12-11 18:42:45 +1000

    feat: add test to get nav links text

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,10 +1,13 @@
 import pytest
-
+from pom.homepage_nav import HomepageNav
 
 
 @pytest.mark.usefixtures('setup')
 class TestHomepage:
 
     def test_homepage(self):
+        homepage_nav = HomepageNav(self.driver)
+        print(homepage_nav.get_nav_links_text())
+
 
 
