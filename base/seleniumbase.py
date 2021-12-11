7f462e6d9f0bafaa264736ff39216b9744f3ade9
Andrew <elbroandrew@gmail.com>
2021-12-11 18:31:46 +1000

    refactor: change method to private

--- base/seleniumbase.py
+++ base/seleniumbase.py
@@ -9,7 +9,7 @@ class SeleniumBase:
         self.driver = driver
         self.wait = WebDriverWait(driver, 15, 0.3)
 
-    def get_selenium_by(self, find_by: str) -> dict:
+    def __get_selenium_by(self, find_by: str) -> dict:
         find_by = find_by.lower()
         locating = {'css': By.CSS_SELECTOR,
                     'xpath': By.XPATH,
@@ -24,25 +24,25 @@ class SeleniumBase:
         return locating[find_by]
 
     def is_visible(self, find_by: str, locator: str, locator_name: str = None) -> WebElement:
-        return self.wait.until(ec.visibility_of_element_located((self.get_selenium_by(find_by), locator)), locator_name)
+        return self.wait.until(ec.visibility_of_element_located((self.__get_selenium_by(find_by), locator)), locator_name)
 
     def is_present(self, find_by: str, locator: str, locator_name: str = None) -> WebElement:
-        return self.wait.until(ec.presence_of_element_located((self.get_selenium_by(find_by), locator)), locator_name)
+        return self.wait.until(ec.presence_of_element_located((self.__get_selenium_by(find_by), locator)), locator_name)
 
     def is_not_present(self, find_by: str, locator: str, locator_name: str = None) -> WebElement:
         return self.wait.until(
-            ec.invisibility_of_element_located((self.get_selenium_by(find_by), locator)),
+            ec.invisibility_of_element_located((self.__get_selenium_by(find_by), locator)),
             locator_name
         )
 
     def are_visible(self, find_by: str, locator: str, locator_name: str = None) -> WebElement:
         return self.wait.until(
-            ec.visibility_of_all_elements_located((self.get_selenium_by(find_by), locator)),
+            ec.visibility_of_all_elements_located((self.__get_selenium_by(find_by), locator)),
             locator_name
         )
 
     def are_present(self, find_by: str, locator: str, locator_name: str = None) -> WebElement:
         return self.wait.until(
-            ec.presence_of_all_elements_located((self.get_selenium_by(find_by), locator)),
+            ec.presence_of_all_elements_located((self.__get_selenium_by(find_by), locator)),
             locator_name
         )
