d268861cf9eba97242c257fff073d73553a25d58
Andrew <elbroandrew@gmail.com>
2021-12-19 00:18:49 +1000

    fix: delete the desired cookie on every click

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -10,17 +10,9 @@ class TestHomepage:
     def test_nav_links(self):
         homepage_nav = HomepageNav(self.driver)
         homepage_nav.get_modal_window_close_button().click()
-        cookies = homepage_nav.driver.get_cookies()
-        cookies_names = [cookie['name'] for cookie in cookies]
-        print(cookies)
-        print('---------------------------------------')
-        print(cookies_names)
+        homepage_nav.driver.delete_cookie('ak_bmsc')
         for idx in range(8):
-            #homepage_nav.get_nav_links()[idx].click()
-            for cookie_name in cookies_names:
-                homepage_nav.driver.delete_cookie(cookie_name)
-                homepage_nav.driver.refresh()
-                homepage_nav.is_visible('tag_name', 'h1', cookie_name)
-            time.sleep(1.5)
+            homepage_nav.get_nav_links()[idx].click()
+            homepage_nav.driver.delete_cookie('ak_bmsc')
 
 
