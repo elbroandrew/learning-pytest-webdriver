2b730cc5a5e1b3fb3dbd332c7725fb7aac348e3e
Andrew <elbroandrew@gmail.com>
2021-12-18 14:25:14 +1000

    refactor: get cookies

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -9,14 +9,19 @@ class TestHomepage:
 
     def test_nav_links(self):
         homepage_nav = HomepageNav(self.driver)
+        cookies = homepage_nav.driver.get_cookies()
+        cookies_names = [cookie['name'] for cookie in cookies]
+        print(cookies)
+        print('---------------------------------------')
+        print(cookies_names)
         homepage_nav.get_modal_window_close_button().click()
-        actual_links = homepage_nav.get_nav_links_text()
-        expected_links = homepage_nav.NAV_LINKS_TEXT
-        assert expected_links == actual_links, 'Validating nav links'
+        # actual_links = homepage_nav.get_nav_links_text()
+        # expected_links = homepage_nav.NAV_LINKS_TEXT
+        # assert expected_links == actual_links, 'Validating nav links'
         #homepage_nav.get_nav_link_by_name('Home').click()
         for idx in range(8):
             homepage_nav.get_nav_links()[idx].click()
-            homepage_nav.driver.delete_all_cookies()
+            #homepage_nav.driver.delete_all_cookies()
             time.sleep(2)
 
 
